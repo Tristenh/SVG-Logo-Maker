8ebb777be802c5770e7d03fec7cc5081331e0dae
Tristenh <tristenhobba1@gmail.com>
2023-07-22 10:32:00 +1000

    created circle, square, triangle js files with const name of the shape and return svg as template literals, add writeFile to create svg files, add jest to devDependencies

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 const inquirer = require("inquirer");
 const jest = require("jest");
 const fs = require("fs");
-inquirer.prompt([
-  {
-    type: "input",
-    name: "title",
-    message: "your project name",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "your project name",
+    },
+  ])
+  .then((data) => {
+    const filename = "shape.svg";
+    const dataInfo = `
+<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+<rect width="200" height="200" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+Sorry, your browser does not support inline SVG.
+</svg>`;
+    //  write README file
+    fs.writeFile(filename, dataInfo, (err) =>
+      err ? console.log(err) : console.log(`success!`)
+    );
+  });
