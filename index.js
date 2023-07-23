ef0decfd15aed8bf382f424f99166a03d0f6c217
Tristenh <tristenhobba1@gmail.com>
2023-07-23 13:33:58 +1000

    prompt title is now a list, with shape choices, if a shape is selected console logs the name of selected shape

--- index.js
+++ index.js
@@ -2,22 +2,31 @@
 const inquirer = require("inquirer");
 const jest = require("jest");
 const fs = require("fs");
+
 inquirer
   .prompt([
     {
-      type: "input",
+      type: "list",
       name: "title",
-      message: "your project name",
+      message: "please select circle, square or triangle",
+      choices: ["circle", "square", "triangle"],
     },
   ])
   .then((data) => {
+    if (data.title.includes("circle")) {
+      console.log("circle");
+    } else if (data.title.includes("square")) {
+      console.log("square");
+    } else if (data.title.includes("triangle")) {
+      console.log("triangle");
+    }
     const filename = "shape.svg";
     const dataInfo = `
-<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-<rect width="200" height="200" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
-Sorry, your browser does not support inline SVG.
-</svg>`;
-    //  write README file
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    <rect width="200" height="200" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+    Sorry, your browser does not support inline SVG.
+    </svg>`;
+    //  write SVG file
     fs.writeFile(filename, dataInfo, (err) =>
       err ? console.log(err) : console.log(`success!`)
     );
